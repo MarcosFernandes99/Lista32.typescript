b387c6f2be85c0478990e5e52b363df4e0e3673b
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-10-07 23:45:33 -0300

    Finalizando o exercicio 2 com todas validações

--- Lista32.js
+++ Lista32.js
@@ -1,17 +1,31 @@
 // EXERCICIO 1
-var frase = "Eu&&$ est%$udo n$a Dod$%ev";
-console.log("A maior palavra e | " + VerificarMaiorPalavra(frase));
-function VerificarMaiorPalavra(fraseParametro) {
-    var palavras = fraseParametro.split(" ");
-    palavras.forEach(function (x, index) {
-        palavras[index] = x.replace(/\W/g, "");
-    });
-    console.log(palavras);
-    var maiorPalavra = "";
-    palavras.forEach(function (x) {
-        if (x.length > maiorPalavra.length) {
-            maiorPalavra = x;
-        }
-    });
-    return maiorPalavra;
+// let frase : string = "Eu&&$ est%$udo n$a Dod$%ev"
+// console.log("A maior palavra e | " + VerificarMaiorPalavra(frase))
+// function VerificarMaiorPalavra(fraseParametro : string): string{
+//     let palavras : string[] = fraseParametro.split(" ")
+//     palavras.forEach((x, index) => {
+//         palavras[index] = x.replace(/\W/g, "")})
+//         console.log(palavras)
+//     let maiorPalavra : string = ""    
+//     palavras.forEach(x => {
+//         if(x.length > maiorPalavra.length){
+//             maiorPalavra = x
+//         }
+//     })
+//     return maiorPalavra  
+// }
+// EXERCICIO 2
+var nome = "Marcos_2022";
+console.log(ValidarNome(nome));
+function ValidarNome(nome) {
+    var primeiroCaractere = /^[a-z]/i;
+    var caracateresValidos = /\W/;
+    var ultimoCaractere = /[^_]/;
+    var teste1 = primeiroCaractere.test(nome);
+    var teste2 = caracateresValidos.test(nome);
+    var teste3 = ultimoCaractere.test(nome);
+    if (nome.length >= 4 && nome.length <= 25 && teste1 && !teste2 && teste3) {
+        return true;
+    }
+    return false;
 }
--- Lista32.ts
+++ Lista32.ts
@@ -1,21 +1,42 @@
 // EXERCICIO 1
 
-let frase : string = "Eu&&$ est%$udo n$a Dod$%ev"
+// let frase : string = "Eu&&$ est%$udo n$a Dod$%ev"
 
-console.log("A maior palavra e | " + VerificarMaiorPalavra(frase))
+// console.log("A maior palavra e | " + VerificarMaiorPalavra(frase))
 
 
-function VerificarMaiorPalavra(fraseParametro : string): string{
-    let palavras : string[] = fraseParametro.split(" ")
-    palavras.forEach((x, index) => {
-        palavras[index] = x.replace(/\W/g, "")})
-        console.log(palavras)
+// function VerificarMaiorPalavra(fraseParametro : string): string{
+//     let palavras : string[] = fraseParametro.split(" ")
+//     palavras.forEach((x, index) => {
+//         palavras[index] = x.replace(/\W/g, "")})
+//         console.log(palavras)
 
-    let maiorPalavra : string = ""    
-    palavras.forEach(x => {
-        if(x.length > maiorPalavra.length){
-            maiorPalavra = x
-        }
-    })
-    return maiorPalavra  
+//     let maiorPalavra : string = ""    
+//     palavras.forEach(x => {
+//         if(x.length > maiorPalavra.length){
+//             maiorPalavra = x
+//         }
+//     })
+//     return maiorPalavra  
+// }
+
+// EXERCICIO 2
+
+let nome : string = "Marcos_2022"
+
+console.log(ValidarNome(nome))
+
+function ValidarNome(nome : string) : boolean{
+    let primeiroCaractere : RegExp = /^[a-z]/i
+    let caracateresValidos : RegExp = /\W/
+    let ultimoCaractere : RegExp = /[^_]/
+
+    let teste1: boolean = primeiroCaractere.test(nome)
+    let teste2: boolean = caracateresValidos.test(nome) 
+    let teste3: boolean = ultimoCaractere.test(nome)
+    
+    if(nome.length >= 4 && nome.length <= 25 && teste1 && !teste2 && teste3){
+        return true
+    }
+    return false
 }
